668974101653992ba747e76882fdbffa3c39a131
zhangks <zhangks93@outlook.com>
2026-02-20 20:11:30 +0800

    use devtools

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -3,6 +3,15 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            #[cfg(feature = "devtools")]
+            {
+                if let Ok(window) = app.get_webview_window("main") {
+                    window.open_devtools();
+                }
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
