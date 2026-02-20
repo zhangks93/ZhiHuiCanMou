295803c05d205ce6990b3b336d8d37e6f33aff7c
zhangks <zhangks93@outlook.com>
2026-02-20 20:19:10 +0800

    use devtools

---
+++ app/src-tauri/target/release/build/syn-7969804532ed7a9c/build_script_build-7969804532ed7a9c.d
@@ -0,0 +1,5 @@
+D:\Code\ZhiHuiCanMou\app\src-tauri\target\release\build\syn-7969804532ed7a9c\build_script_build-7969804532ed7a9c.d: C:\Users\HLJY\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
+
+D:\Code\ZhiHuiCanMou\app\src-tauri\target\release\build\syn-7969804532ed7a9c\build_script_build-7969804532ed7a9c.exe: C:\Users\HLJY\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
+
+C:\Users\HLJY\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs:
