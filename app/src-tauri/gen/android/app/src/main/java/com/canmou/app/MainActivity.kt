cb2a63dbc741bfe681403fab8f75825d32aabb6b
zhangks <zhangks93@outlook.com>
2026-02-24 21:49:04 +0800

    add android release yml

---
+++ app/src-tauri/gen/android/app/src/main/java/com/canmou/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.canmou.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
